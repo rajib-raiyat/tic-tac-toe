6a28581df9046376f1356ac583a0e9bb6d1c5147
rajib-raiyat <rajib2@qmail.com.bd>
2020-09-16 02:07:59 +0600

    Create multiple function for reduce code complexity
        + change data store process from print to array
        + invalid input checking

--- main.c
+++ main.c
@@ -5,19 +5,14 @@
 
 #include <stdio.h>
 
-// This function is for create an empty board for game.
-void create_the_empty_board(board_size){
-    // board_size -> size of the board [value type int]
-    for(int i=0; i<board_size; i++){
-        for(int j=0; j<board_size; j++){
-            printf(".");
-        }
-        printf("\n");
-    }
-}
+int create_game_array(int board_size);
+int check_input(int board_size, int input);
+void player_1_turn(int board_size, int player);
+void view_game_array(int board_size, int game_array[20][20]);
 
 int main() {
     int size_fo_board;
+
     printf("What is the size of the board you want to play in?\n");
     scanf("%d", &size_fo_board); // Take the size of the board.
 
@@ -31,7 +26,80 @@ int main() {
         return 0;
     }
 
-    create_the_empty_board(size_fo_board);
+    create_game_array(size_fo_board);
+    printf("Player 1's turn [e.g 1 2]\n");
+    player_1_turn(size_fo_board, 1);
+
+    printf("Player 2's turn [e.g 1 1]\n");
+    player_1_turn(size_fo_board, 2);
 
     return 0;
+}
+
+void view_game_array(int board_size, int game_array[20][20]){
+    for(int i = 0; i < board_size; i++)
+    {
+        for(int j = 0; j < board_size; j++)
+        {
+            printf("%c", game_array[i][j]);
+        }
+        printf("\n");
+    }
+}
+int check_input(int board_size, int input) {
+    if (input > board_size || input < 1){
+        return 1;
+    }
+    return 0;
+}
+
+
+// This function is for create an empty board for game.
+int create_game_array(int board_size){
+    int game_array[20][20];
+
+    for (int i=0; i<board_size; i++){
+        for (int j=0; j<board_size; j++){
+            game_array[i][j] = '.';
+        }
+    }
+
+    view_game_array(board_size, game_array);
+
+    return 0;
+}
+
+
+void player_1_turn(int board_size, int player){
+    int flag = 0;
+    int pi, pj;
+
+    scanf("%d %d", &pi, &pj);
+
+    while (flag == 0){
+        if (check_input(board_size, pi) != 0 || check_input(board_size, pj) != 0 ){
+            printf("%d, %d is outside the board.\n", pi, pj);
+            printf("Please try again\n");
+            scanf("%d %d", &pi, &pj);
+
+        } else {
+            flag = 1;
+        }
+    }
+
+    for (int i = 0; i < board_size; i++) {
+        for (int j = 0; j < board_size; j++) {
+            if (i == pi-1 && j == pj-1) {
+                if (player == 1){
+                    printf("X");
+                } else {
+                    printf("O");
+                }
+            } else {
+                printf(".");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
