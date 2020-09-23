3e0b5c92349ed728993ef1fabdbe2339a32fad8a
rajib-raiyat <rajib2@qmail.com.bd>
2020-09-23 09:57:28 +0600

    FIX :: game value reset to `.` from 3rd turn
        > multiple turn (bug not working properly)
        > typo fix
        + check win function (WIP)

--- main.c
+++ main.c
@@ -1,50 +1,57 @@
 /*
- * Check out github repository
- * https://github.com/rajib-raiyat/tic-tac-toe
- */
+* Check out github repository
+* https://github.com/rajib-raiyat/tic-tac-toe
+*/
 
 #include <stdio.h>
 
 int create_game_array(int board_size);
+
 int check_input(int board_size, int input);
+
+int check_win(int board_size, char game_array[20][20]);
 
 void make_turn(int board_size, int player);
 
-void view_game_array(int board_size, int game_array[20][20]);
+void view_game_array(int board_size, char game_array[20][20]);
 
 int main() {
-    int flag = 0;
-    int size_fo_board;
+    int flag = 0, turn_count = 1;
+    int size_of_board;
 
     printf("What is the size of the board you want to play in?\n");
 
     while (flag == 0) {
-        scanf("%d", &size_fo_board); // Take the size of the board.
+        scanf("%d", &size_of_board); // Take the size of the board.
 
-        if (size_fo_board < 3) {
+        if (size_of_board < 3) {
             printf("Minimum size of board must be 3.\nPlease try again!\n");
-        } else if (size_fo_board > 15) {
+        } else if (size_of_board > 15) {
             printf("Maximum size of board must be 15.\nPlease try again!\n");
         } else {
             flag = 1;
         }
     }
 
-    create_game_array(size_fo_board);
-    printf("Player 1's turn [e.g 1 2]\n");
-    make_turn(size_fo_board, 1);
+    create_game_array(size_of_board);
 
-    printf("Player 2's turn [e.g 1 1]\n");
-    make_turn(size_fo_board, 2);
+    while (turn_count != (size_of_board * size_of_board)) {
+        turn_count++;
+
+        printf("Player 1's turn [e.g 1 2]\n");
+        make_turn(size_of_board, 1);
+
+        printf("Player 2's turn [e.g 1 1]\n");
+        make_turn(size_of_board, 2);
+    }
+
 
     return 0;
 }
 
-void view_game_array(int board_size, int game_array[20][20]) {
-    for(int i = 0; i < board_size; i++)
-    {
-        for(int j = 0; j < board_size; j++)
-        {
+void view_game_array(int board_size, char game_array[20][20]) {
+    for (int i = 0; i < board_size; i++) {
+        for (int j = 0; j < board_size; j++) {
             printf("%c ", game_array[i][j]);
         }
         printf("\n");
@@ -60,7 +67,7 @@ int check_input(int board_size, int input) {
 
 // This function is for create an empty board for game.
 int create_game_array(int board_size){
-    int game_array[20][20];
+    char game_array[20][20];
 
     for (int i=0; i<board_size; i++) {
         for (int j=0; j<board_size; j++) {
@@ -78,9 +85,9 @@ void make_turn(int board_size, int player) {
     int flag = 0;
     int pi, pj;
 
-    if (game_array[1][1] != '.') {
-        for (int i = 0; i < board_size; i++) {
-            for (int j = 0; j < board_size; j++) {
+    for (int i = 0; i < board_size; i++) {
+        for (int j = 0; j < board_size; j++) {
+            if ((game_array[i][j] != '.') && (game_array[i][j] != 'X') && (game_array[i][j] != 'O')) {
                 game_array[i][j] = '.';
             }
         }
@@ -88,7 +95,6 @@ void make_turn(int board_size, int player) {
 
     while (flag == 0) {
         scanf("%d %d", &pi, &pj);
-
         if (check_input(board_size, pi) != 0 || check_input(board_size, pj) != 0) {
             printf("%d, %d is outside the board.\n", pi, pj);
             printf("Please try again\n");
@@ -109,12 +115,21 @@ void make_turn(int board_size, int player) {
         }
     }
 
-    for (int i = 0; i < board_size; i++) {
-        for (int j = 0; j < board_size; j++) {
-            printf("%c ", game_array[i][j]);
+    view_game_array(board_size, game_array);
+    check_win(board_size, game_array);
+}
+
+int check_win(int board_size, char game_array[20][20]) {
+    int won = 0; // boolean (0/1).
+
+    for (int i = 0; i < board_size && !won; i++) {
+        int j = 0;
+
+        while (j < board_size && game_array[i][j] == 'X') {
+            ++j;
+            won = j == board_size;
         }
-        printf("\n");
     }
 
-    printf("\n");
+    return won;
 }
