91b28a990c24a9f01083a6754e63a87fb1a10f63
rajib-raiyat <rajib2@qmail.com.bd>
2020-09-24 15:27:56 +0600

    FIX :: check win issue and game logic
        + game over ASCII art
        ! game result always check 3 matches

--- main.c
+++ main.c
@@ -4,20 +4,21 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int create_game_array(int board_size);
 
 int check_input(int board_size, int input);
 
-int check_win(int board_size, char game_array[20][20]);
+void check_win(int board_size, char game_array[20][20]);
 
 void make_turn(int board_size, int player);
 
 void view_game_array(int board_size, char game_array[20][20]);
 
 int main() {
-    int flag = 0, turn_count = 1;
-    int size_of_board;
+    int flag = 0;
+    int size_of_board = 0;
 
     printf("What is the size of the board you want to play in?\n");
 
@@ -35,27 +36,37 @@ int main() {
 
     create_game_array(size_of_board);
 
-    while (turn_count != (size_of_board * size_of_board)) {
-        turn_count++;
-
-        printf("Player 1's turn [e.g 1 2]\n");
+    for (int i = 1; i <= (size_of_board + 2); i++) {
         make_turn(size_of_board, 1);
+
+        if (i == size_of_board + 2) {
+            break;
+        }
 
         printf("Player 2's turn [e.g 1 1]\n");
         make_turn(size_of_board, 2);
     }
 
-
     return 0;
 }
 
 void view_game_array(int board_size, char game_array[20][20]) {
+    char temp_array[20][20];
+
     for (int i = 0; i < board_size; i++) {
         for (int j = 0; j < board_size; j++) {
             printf("%c ", game_array[i][j]);
         }
         printf("\n");
     }
+
+    for (int i = 0; i < board_size; i++) {
+        for (int j = 0; j < board_size; j++) {
+            temp_array[i][j] = game_array[i][j];
+        }
+    }
+
+    check_win(board_size, temp_array);
 }
 
 int check_input(int board_size, int input) {
@@ -82,6 +93,7 @@ int create_game_array(int board_size){
 
 void make_turn(int board_size, int player) {
     char game_array[20][20];
+
     int flag = 0;
     int pi, pj;
 
@@ -116,32 +128,43 @@ void make_turn(int board_size, int player) {
     }
 
     view_game_array(board_size, game_array);
-    check_win(board_size, game_array);
 }
 
-int check_win(int board_size, char game_array[20][20]) {
-    int flag = 0;
-    for (int i = 0; i < board_size; i++) {
-        int j = 0;
-        for (int k = j + 1; k < board_size; k++) {
-            if (game_array[i][j] != game_array[i][k]) {
-                flag = 1;
-                break;
-            } else {
-                flag = 0;
+
+void check_win(int board_size, char game_array[20][20]) {
+    int i, j, flag = 0;
+    for (i = 0; i < board_size; i++) {
+        for (j = 0; j < board_size; j++) {
+            if (game_array[i][j] == 'X' || game_array[i][j] == 'O') {
+                if ((game_array[i][j] == game_array[i][j - 1] && game_array[i][j] == game_array[i][j + 1]) ||
+                    (game_array[i][j] == game_array[i - 1][j] && game_array[i][j] == game_array[i + 1][j]) ||
+                    (game_array[i][j] == game_array[i - 1][j + 1] && game_array[i][j] == game_array[i + 1][j - 1]) ||
+                    (game_array[i][j] == game_array[i - 1][j - 1] && game_array[i][j] == game_array[i + 1][j + 1])) {
+                    if (game_array[i][j] == 'X') {
+                        printf("Player 1 win\n");
+                        flag = 1;
+                    } else if (game_array[i][j] == 'O') {
+                        printf("Player 2 win\n");
+                        flag = 1;
+                    } else {
+                        printf("Draw\n");
+                        flag = 1;
+                    }
+                }
             }
         }
-
-        if (flag == 0) {
-            break;
-        }
     }
 
-    if (flag == 0) {
-        printf("Match found");
-    } else {
-        printf("Match not found");
+    if (flag == 1) {
+        printf("\n"
+               "\n"
+               "   _____          __  __ ______     ______      ________ _____     _    _ \n"
+               "  / ____|   /\\   |  \\/  |  ____|   / __ \\ \\    / /  ____|  __ \\   | |  | |\n"
+               " | |  __   /  \\  | \\  / | |__     | |  | \\ \\  / /| |__  | |__) |  | |  | |\n"
+               " | | |_ | / /\\ \\ | |\\/| |  __|    | |  | |\\ \\/ / |  __| |  _  /   | |  | |\n"
+               " | |__| |/ ____ \\| |  | | |____   | |__| | \\  /  | |____| | \\ \\   |_|  |_|\n"
+               "  \\_____/_/    \\_\\_|  |_|______|   \\____/   \\/   |______|_|  \\_\\  (_)  (_)\n"
+               "");
+        exit(0);
     }
-
-    return flag;
 }
